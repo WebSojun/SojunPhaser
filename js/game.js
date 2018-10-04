3e5c34fd661f259c7e2bd48fe87a28f568e8ee31
nanen123 <lkjhgf13579@naver.com>
2018-10-04 19:54:34 +0900

    add spritesheet

--- js/game.js
+++ js/game.js
@@ -1,13 +1,18 @@
 var game = new Phaser.Game(800,600,Phaser.Auto)
 
-var TEST = {
+var Game = {
     preload: function(){
-
+        game.load.spritesheet('Player','../asset/sprites/RunP.png',57,80,12);
     },
     create: function(){
-
+        var Player = game.add.sprite(100,100,'Player');
+        var Run = Player.animations.add('Run')
+        Player.animations.play('Run',20,true);
     },
     update: function(){
         
     }
 }
+game.state.add('Game',Game);
+
+game.state.start('Game');
