b4ea6ae2975dd26e4161bd2c85f831a8771cee27
nanen123 <lkjhgf13579@naver.com>
2018-10-04 20:35:06 +0900

    add runAnim

--- js/game.js
+++ js/game.js
@@ -2,11 +2,15 @@ var game = new Phaser.Game(800,600,Phaser.Auto)
 
 var Game = {
     preload: function(){
-        game.load.spritesheet('Player','../asset/sprites/RunP.png',55,70,12);
+        game.load.image('BG','../asset/background/background.png');
+        game.load.image('Obstacle','../asset/sprites/Obstacle.png')
+        game.load.spritesheet('Player','../asset/sprites/RunP.png',55,64,12);
     },
     create: function(){
-        var Player = game.add.sprite(100,100,'Player');
-        var RunAnim = Player.animations.add('Run')
+        this.BG = game.add.image(0,0,'BG')
+        this.Player = game.add.sprite(100,100,'Player');
+        this.Obstacle = game.add.image(500,100,'Obstacle');
+        this.RunAnim = Player.animations.add('Run')
         Player.animations.play('Run',20,true);
     },
     update: function(){
