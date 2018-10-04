e1cfded23ac6188ddf9ef6b15b2544f72189237c
nanen123 <lkjhgf13579@naver.com>
2018-10-04 20:16:32 +0900

    fix Player Image

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ var game = new Phaser.Game(800,600,Phaser.Auto)
 
 var Game = {
     preload: function(){
-        game.load.spritesheet('Player','../asset/sprites/RunP.png',55,80,12);
+        game.load.spritesheet('Player','../asset/sprites/RunP.png',55,70,12);
     },
     create: function(){
         var Player = game.add.sprite(100,100,'Player');
