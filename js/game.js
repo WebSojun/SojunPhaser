03d458cd4de6d80747169142eb570fd7e0386357
nanen123 <lkjhgf13579@naver.com>
2018-10-04 20:11:14 +0900

    fix animation

--- js/game.js
+++ js/game.js
@@ -2,11 +2,11 @@ var game = new Phaser.Game(800,600,Phaser.Auto)
 
 var Game = {
     preload: function(){
-        game.load.spritesheet('Player','../asset/sprites/RunP.png',57,80,12);
+        game.load.spritesheet('Player','../asset/sprites/RunP.png',55,80,12);
     },
     create: function(){
         var Player = game.add.sprite(100,100,'Player');
-        var Run = Player.animations.add('Run')
+        var RunAnim = Player.animations.add('Run')
         Player.animations.play('Run',20,true);
     },
     update: function(){
