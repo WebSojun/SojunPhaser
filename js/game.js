7bd2ddd7ecd0d233ebc73ff3ed0d35d83193a4a5
yuicraft <twins20131988@gmail.com>
2018-10-11 19:34:12 +0900

    commit

--- js/game.js
+++ js/game.js
@@ -12,11 +12,12 @@ var Game = {
         game.load.image('BG', '../asset/background/BG.png');
         game.load.image('Obstacle','../asset/sprites/Obstacle.png')
         game.load.spritesheet('Player','../asset/sprites/RunP.png',55,64,12);
+        game.load.
     },
 
     create: function(){
         //  Set game physics
-        this.physics.startSystem(Phaser.physics.ARCADE);
+        game.physics.startSystem(Phaser.physics.ARCADE);
         game.physics.arcade.gravity.y = 250;
 
         //  Keyboard
