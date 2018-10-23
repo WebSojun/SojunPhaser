6ee483604381d3cb75c66a05e42c9bb04f8cb640
nanen123 <lkjhgf13579@naver.com>
2018-10-23 20:17:13 +0900

    add God Time

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ var game = new Phaser.Game(WIDTH, HEIGHT,Phaser.Auto)
 var Game = {
     preload: function(){
         game.load.image('BG', '../asset/background/BG.png');
-        game.load.image('Obstacle','../asset/sprites/Obstacle.png')
+        game.load.image('Obstacle','../asset/sprites/Obstacle.png');
         game.load.image('BigBlock','../asset/blocks/High.png');
         game.load.image('SmallBlock','../asset/blocks/Low.png');
         game.load.spritesheet('Player','../asset/sprites/RunP.png',55,64,12);
@@ -46,10 +46,14 @@ var Game = {
         this.Player.body.setSize(20, 32, 5, 16);
 
         //#endregion
+        //#region Setting ObsGroup
         this.ObsGroup = game.add.group();
         this.ObsGroup.physicsBodyType = Phaser.Physics.ARCADE;
         this.ObsGroup.enableBody = true;
-
+        //#endregion
+        this.PlayerHp = 3;
+        this.GodTime = 1;
+        this.PlayerTime = 0;
     },
 
     BG_effect: function() {
@@ -64,28 +68,28 @@ var Game = {
 
 
     update: function(){
+        this.PlayerTime += 1.0/60.0;
         Game.BG_effect();
 
         this.pushObs();
-       // this.pushBig();
-        //this.pushSmall();
-        // for(let i=0; i<this.ObsList.length;i++){
-        //     this.ObsGroup[i].x-=5;
-        // }
-        
         if(game.physics.arcade.collide(Game.Player,Game.Floor) && Game.jumpButton.isDown){
             Game.Player.body.velocity.y = -500;
         }
 
-        if(game.physics.arcade.collide(Game.Player,this.ObsGroup)){
-            game.state.start('TEMP');
+        if(game.physics.arcade.collide(Game.Player,this.ObsGroup) && this.PlayerTime > this.GodTime){
+            Game.PlayerHp -= 1;
+            this.PlayerTime = 0;
         }
 
+        if(this.PlayerHp <= 0){
+            game.state.start('TEMP');
+        }
         this.ObsGroup.subAll('x',5);
     },
 
+    //장애물 소환
     pushObs: function(){
-        if(rand(1,40) == 1){
+        if(rand(1,100) == 1){
             this.ObsGroup.create(800,460,'Obstacle');
             this.ObsGroup.setAll('body.allowGravity', false);
             this.ObsGroup.setAll('body.immovable', true);
